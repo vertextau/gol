c53f029788aaa718caff2a00f67b74dd7801f50c
vertextau <43003542+vertextau@users.noreply.github.com>
2019-01-27 13:59:16 +0200

    Unroll the imports

--- src/main/java/com/vertextau/gol/UI.java
+++ src/main/java/com/vertextau/gol/UI.java
@@ -1,8 +1,17 @@
 package com.vertextau.gol;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JFormattedTextField;
+import javax.swing.JToolBar;
+import javax.swing.SwingUtilities;
 
-import java.awt.*;
+import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.BorderLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
